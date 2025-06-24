cca32eab781ace40c6839f357e368ae3a7ae965c
DDX5 <51382988+DDX5@users.noreply.github.com>
2025-06-24 14:17:47 +0100

    Updated files 24/06/2025 14:17

--- src/JTPoll/util/SecurityManager.py
+++ src/JTPoll/util/SecurityManager.py
@@ -2,7 +2,6 @@ import base64, hashlib, secrets, aes, aes_cipher
 from aes.utils import *
 from aes_cipher import Pbkdf2Sha512
 
-
 def generateKey(password,installFile):
     sha3_512 = hashlib.sha3_512()
     sha3_256 = hashlib.sha3_256()
@@ -17,7 +16,7 @@ def generateKey(password,installFile):
     mainKey = sha3_256.digest()
     mainIV = shake_256.digest(16)
     crypt = aes.aes(int2arr8bit(bytes2int(mainKey),32), 256,"CBC", "PKCS#7", int2arr8bit(bytes2int(mainIV),16))
-    mainKey,mainIV,shake_256, sha3_512, sha3_256, pdb = None,None,None,None,None,None
+    mainKey,mainIV,shake_256, sha3_512, sha3_256, pdb, password = None,None,None,None,None,None,None
     secureKey = secrets.token_bytes(32)
     secureIV = secrets.token_bytes(16)
     secureKey = crypt.enc(int2arr8bit(bytes2int(secureKey),32))
@@ -30,3 +29,23 @@ def generateKey(password,installFile):
     writer.write(secureKey + '\n' + secureIV + '\n' + verificationCheck)
     writer.flush()
     writer.close()
+
+def CheckPassword(password,installFile):
+    sha3_512 = hashlib.sha3_512()
+    sha3_256 = hashlib.sha3_256()
+    shake_256 = hashlib.shake_256()
+    pdb = Pbkdf2Sha512(1024 * 1024)
+    sha3_512.update(password)
+    salt = sha3_512.digest()
+    preKey = pdb.DeriveKey(password, salt)
+    sha3_256.update(preKey)
+    shake_256.update(preKey)
+    mainKey = sha3_256.digest()
+    mainIV = shake_256.digest(16)
+    crypt = aes.aes(int2arr8bit(bytes2int(mainKey), 32), 256, "CBC", "PKCS#7", int2arr8bit(bytes2int(mainIV), 16))
+    mainKey, mainIV, shake_256, sha3_512, sha3_256, pdb, password = None, None, None, None, None, None, None
+    verificationCheck = installFile.open("r").readlines()[2]
+    verificationCheckLen = len(base64.b64decode(verificationCheck.encode()))
+    verificationCheck = crypt.dec(int2arr8bit(bytes2int(base64.b64decode(verificationCheck.encode())),verificationCheckLen))
+    verificationCheck = int2bytes(arr8bit2int(verificationCheck)).decode()
+    return verificationCheck == "JTPoll"
